fb2a39d93e0989b31c61eadebbaea2a0c6c41b80
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2020-07-30 12:41:41 -0400

    Final Changes!

--- MMSS_cosmology.py
+++ MMSS_cosmology.py
@@ -13,7 +13,7 @@ mpl.rcParams['ytick.major.size'], mpl.rcParams['ytick.minor.size'] = 14, 8
 mpl.rcParams['ytick.major.width'], mpl.rcParams['ytick.minor.width'] = 1.2, 0.8
 
 #Command to compute Omega
-def Omega_X(a, Omega_0, w):
+def Omega_X(a, Omega_0, w_func):
     '''
     Computes the fraction of energy in the Universe due to a component X.
 
@@ -25,7 +25,7 @@ def Omega_X(a, Omega_0, w):
     Omega_0: float
         The value of Omega_X at present (a = 1)
 
-    w: float, int, or function of a
+    w_func: float, int, or function of a
         The equation of state w = pressure/energy of the component X.
         In this project, we make it a function of scale factor a
 
@@ -38,6 +38,14 @@ def Omega_X(a, Omega_0, w):
 
     #Initialize numpy array to store our output
     factor = np.zeros(a.size)
+
+    #If given an int/float, rewrite it as a function
+    try:
+        w_func(0)
+        w = w_func
+    except:
+        w = lambda x: w_func
+
 
     #Iterate/loop over all input values of a
     for i, a_now in enumerate(a):
@@ -61,9 +69,9 @@ def Our_Universe(a_start = 1e-7, a_end = 1e2, Omega_m = 0.3, Omega_r = 2e-5, Ome
 
     a = np.geomspace(a_start, a_end, 500, endpoint=True)
 
-    Omega_M = Omega_X(a, Omega_m,  lambda x: w_m)
-    Omega_R = Omega_X(a, Omega_r, lambda x: w_r)
-    Omega_L = Omega_X(a, Omega_l,  lambda x: w_Lambda)
+    Omega_M = Omega_X(a, Omega_m,  w_m)
+    Omega_R = Omega_X(a, Omega_r,  w_r)
+    Omega_L = Omega_X(a, Omega_l,  w_Lambda)
 
     Normalize = (Omega_M + Omega_R + Omega_L)
 
@@ -84,11 +92,11 @@ def Our_Universe(a_start = 1e-7, a_end = 1e2, Omega_m = 0.3, Omega_r = 2e-5, Ome
     axes[1].set_yscale('log')
     [ax.set_xscale('log') for ax in axes]
 
-    axes[0].plot(a, Omega_M/Normalize, lw = 2, label = r'$\Omega_m,\, w = ' + str(w_m) + '$')
-    axes[0].plot(a, Omega_R/Normalize, lw = 2, label = r'$\Omega_r,\, w = ' + str(w_r) + '$')
-    axes[0].plot(a, Omega_L/Normalize, lw = 2, label = r'$\Omega_\Lambda,\, w = ' + str(w_Lambda) + '$')
+    axes[0].plot(a, Omega_M/Normalize, lw = 2, label = r'$\Omega_m$')
+    axes[0].plot(a, Omega_R/Normalize, lw = 2, label = r'$\Omega_r$')
+    axes[0].plot(a, Omega_L/Normalize, lw = 2, label = r'$\Omega_\Lambda$')
 
-    axes[0].legend(fontsize = 20)
+    axes[0].legend(fontsize = 24)
     axes[0].set_ylabel(r'$\Omega_i(a)$', size = 30)
     axes[0].grid()
     axes[0].axvline(1, alpha = 0.3, lw = 2, ls = '--', c = 'k')
@@ -105,9 +113,9 @@ def New_Universe(w_new, Omega_new, name = 'new', a_start = 1e-7, a_end = 1e2, Om
 
     a = np.geomspace(a_start, a_end, 500, endpoint=True)
 
-    Omega_M = Omega_X(a, Omega_m,  lambda x: w_m)
-    Omega_R = Omega_X(a, Omega_r,  lambda x: w_r)
-    Omega_L = Omega_X(a, Omega_l,  lambda x: w_Lambda)
+    Omega_M = Omega_X(a, Omega_m,  w_m)
+    Omega_R = Omega_X(a, Omega_r,  w_r)
+    Omega_L = Omega_X(a, Omega_l,  w_Lambda)
     Omega_New = Omega_X(a, Omega_new, w_new)
 
     Normalize = (Omega_M + Omega_R + Omega_L + Omega_New)
@@ -130,12 +138,12 @@ def New_Universe(w_new, Omega_new, name = 'new', a_start = 1e-7, a_end = 1e2, Om
     axes[1].set_yscale('log')
     [ax.set_xscale('log') for ax in axes]
 
-    axes[0].plot(a, Omega_M/Normalize, lw = 2, label = r'$\Omega_m,\, w = ' + str(w_m) + '$')
-    axes[0].plot(a, Omega_R/Normalize, lw = 2, label = r'$\Omega_r,\, w = ' + str(w_r) + '$')
-    axes[0].plot(a, Omega_L/Normalize, lw = 2, label = r'$\Omega_\Lambda,\, w = ' + str(w_Lambda) + '$')
+    axes[0].plot(a, Omega_M/Normalize, lw = 2, label = r'$\Omega_m$')
+    axes[0].plot(a, Omega_R/Normalize, lw = 2, label = r'$\Omega_r$')
+    axes[0].plot(a, Omega_L/Normalize, lw = 2, label = r'$\Omega_\Lambda$')
     axes[0].plot(a, Omega_New/Normalize, lw = 2, label = r'$\Omega_{\rm ' + name + '}$')
 
-    axes[0].legend(fontsize = 20)
+    axes[0].legend(fontsize = 24)
     axes[0].set_ylabel(r'$\Omega_i(a)$', size = 30)
     axes[0].grid()
 
